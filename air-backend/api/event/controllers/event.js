a96cb9d30b4e968ba4565b1bead19a246cbc6eee
Antonio Vrbančić <avrbancic@foi.hr>
2020-12-01 20:06:33 +0100

    Dodana mogućnost dohvata svih podataka o ulogiranom korisniku, ažuriranja podataka o korisniku, dodani event i event type te komponenta za termin eventa

--- air-backend/api/event/controllers/event.js
+++ air-backend/api/event/controllers/event.js
@@ -1,8 +1,24 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findByUsername(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.users.search(ctx.query);
+    } else {
+      entities = await strapi.services.users.find(ctx.query);
+    }
+    return entities.map(entity => {
+      const usersClasses = sanitizeEntity(entity, {
+        model: strapi.models.users,
+      });
+      return usersClasses;
+    });
+  },
+};
---
+++ air-backend/extensions/users-permissions/controllers/User.js
@@ -0,0 +1,104 @@
+const _ = require('lodash');
+const { sanitizeEntity } = require('strapi-utils');
+
+const sanitizeUser = user =>
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
+
+const formatError = error => [
+    { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+    }
+
+    const userQuery = await strapi.query('user', 'users-permissions');
+    const userWithMedia = await userQuery.findOne({ id: ctx.state.user.id });
+
+    const data = sanitizeUser(userWithMedia, { model: userQuery.model });
+    ctx.send(data);
+  },
+
+  async updateMe(ctx) {
+    const advancedConfigs = await strapi
+      .store({
+        environment: '',
+        type: 'plugin',
+        name: 'users-permissions',
+        key: 'advanced',
+      })
+      .get();
+
+    const { id } = ctx.state.user;
+    const { email, username, password } = ctx.request.body;
+
+    const user = await strapi.plugins['users-permissions'].services.user.fetch({
+      id,
+    });
+
+    if (_.has(ctx.request.body, 'email') && !email) {
+      return ctx.badRequest('email.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'username') && !username) {
+      return ctx.badRequest('username.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'password') && !password && user.provider === 'local') {
+      return ctx.badRequest('password.notNull');
+    }
+
+    if (_.has(ctx.request.body, 'username')) {
+      const userWithSameUsername = await strapi
+        .query('user', 'users-permissions')
+        .findOne({ username });
+
+      if (userWithSameUsername && userWithSameUsername.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: 'Auth.form.error.username.taken',
+            message: 'username.alreadyTaken.',
+            field: ['username'],
+          })
+        );
+      }
+    }
+
+    if (_.has(ctx.request.body, 'email') && advancedConfigs.unique_email) {
+      const userWithSameEmail = await strapi
+        .query('user', 'users-permissions')
+        .findOne({ email: email.toLowerCase() });
+
+      if (userWithSameEmail && userWithSameEmail.id != id) {
+        return ctx.badRequest(
+          null,
+          formatError({
+            id: 'Auth.form.error.email.taken',
+            message: 'Email already taken',
+            field: ['email'],
+          })
+        );
+      }
+      ctx.request.body.email = ctx.request.body.email.toLowerCase();
+    }
+
+    let updateData = {
+      ...ctx.request.body,
+    };
+
+    if (_.has(ctx.request.body, 'password') && password === user.password) {
+      delete updateData.password;
+    }
+
+    const data = await strapi.plugins['users-permissions'].services.user.edit({ id }, updateData);
+
+    ctx.send(sanitizeUser(data));
+  },
+};
