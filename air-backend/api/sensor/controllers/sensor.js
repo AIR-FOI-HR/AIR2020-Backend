1a2076503ca3b5f6e42c3ba5b4458021c78c8002
Antonio Vrbančić <avrbancic@foi.hr>
2021-01-27 21:11:05 +0100

    Fixevi

--- air-backend/api/sensor/controllers/sensor.js
+++ air-backend/api/sensor/controllers/sensor.js
@@ -5,4 +5,13 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    // GET /connection string
+    async getConnectionStrings(ctx) {
+      const result = await strapi
+      .query('sensor')
+      .model.fetchAll({ columns: ['Mark', 'Name'] }) // here we wait for one column only
+      console.log(result)
+      ctx.send(result);
+    },
+};
